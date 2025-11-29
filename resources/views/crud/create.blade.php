8ab87a2be703ffee4e8c35d9c2bf991b8dcce60f
Abdullahi <abdullahialiyugarba14@gmail.com>
2025-11-29 12:42:11 +0100

    another page

--- resources/views/crud/create.blade.php
+++ resources/views/crud/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="min-h-screen bg-gray-50 py-8">
+<div class="min-h-screen bg-gray-50 dark:bg-gray-900 py-8">
     <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
         <!-- Header -->
         <div class="text-center mb-8">
